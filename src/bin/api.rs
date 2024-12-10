6dd381404cf6477c2d488ad427eb1d7d315b28ae
OsmanthusPeace <3139714892@qq.com>
2024-12-10 23:02:14 +0800

    feat: add cors

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,8 +1,18 @@
+use hyper::header::HeaderValue;
+use hyper::Method;
 use my_ledger_backend::api::router::create_router;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    let app = create_router();
+    let allowed_origins = vec![HeaderValue::from_static("http://localhost:5173")];
+    
+    let cors = CorsLayer::new()
+        .allow_origin(allowed_origins)
+        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
+        .allow_headers(Any);
+
+    let app = create_router().layer(cors);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
